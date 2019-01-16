e93b5b1896dd8069a29d463dddc4825b90013b93
Budi Saputra <kursdemy.budisaputra@gmail.com>
2019-01-16 12:22:55 +0700

    MongoDB Mongoose #2

--- server.js
+++ server.js
@@ -12,20 +12,92 @@ mongoose.connect("mongodb://root:Samsung33@ds157544.mlab.com:57544/mongoose",  {
     }
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
-
-app.get("/", function(req, res, next) {
-    res.json("Selamat datang di Halaman Beranda")
-});
-
 var UserSchema = new mongoose.Schema({
     name: String,
     age : Number
 });
 
+UserSchema.methods.addLastName = function(lastName){
+    this.name = this.name + " " + lastName;
+    return this.name;
+};
+
 // Membuat Model
 var User = mongoose.model("User", UserSchema);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// Halaman index root
+// app.get("/", function(req, res, next) {
+//     res.json("Selamat datang di Halaman Beranda")
+// });
+
+
+
+// Menampilkan Database Spesifik
+// app.get("/", function(req, res, next){
+//     User.find({name: "Budi"}, function(err, foundUser){
+//         if (foundUser){
+//             res.json(foundUser);
+//         } else {
+//             res.json("User tidak ada")
+//         }
+//     });
+// });
+
+// Menampilkan Database Keseluruhan
+// app.get("/", function(req, res, next){
+//     User.find({}, function(err, foundUser){
+//         if (foundUser){
+//             res.json(foundUser);
+//         } else {
+//             res.json("User tidak ada")
+//         }
+//     });
+// });
+
+// Menampilkan Database Dynamic
+// app.get("/:name", function(req, res, next){
+//     User.find({ name: req.params.name }, function(err, foundUser){
+//         if (foundUser){
+//             res.json(foundUser);
+//         } else {
+//             res.json("User tidak ada")
+//         }
+//     });
+// });
+
+// Menampikan database Dynamic dengan Pesan Eror
+// app.get("/:name", function(req, res, next){
+//     User.findOne({ name: req.params.name }, function(err, foundUser){
+//         if (foundUser){
+//             res.json(foundUser);
+//         } else {
+//             res.json("User tidak ada")
+//         }
+//     });
+// });
+
+// Menampilkan database dengan Id
+// app.get("/:id", function(req, res, next){
+//     User.findOne({ _id: req.params.id}, function(err, foundUser){
+//         if (foundUser){
+//             res.json(foundUser);
+//         } else {
+//             res.json("User tidak ada")
+//         }
+//     });
+// });
+
+app.get("/:id", function(req, res, next){
+    User.findById({ _id: req.params.id}, function(err, foundUser){
+       foundUser.addLastName("Saputra");
+       foundUser.save(function(err){
+           res.json(foundUser)
+       });
+    });
+});
 
 // Membuat New User 
 // app.get("/create-user", function(req, res, next){
